6a58a269e0008d4c41c2e0c8a4f7787a638f46b2
Sergio Merino Hernández <sergiohdez94@gmail.com>
2017-03-09 14:55:14 +0100

    Update

--- starterkit.js
+++ starterkit.js
@@ -6,10 +6,33 @@ jQuery(document).ready(function() {
   $("#rating").addClass("blue");
   $("#large").addClass("greenbg");
   $("#orderedlist2").addClass("bluebg");
+  $("#orderedlist").find("li").each(function(i) {
+    $(this).append(" Line number " + i);
+  });
+  $("#orderedlist2 > li:last > ul > li").hover(function() {
+    $(this).append(" Chao");
+  }, function() {
+    $(this).append(" pescao!");
+  });
   $("#orderedlist2 > li").addClass("red");
   $("#orderedlist2 > li:last").hover(function() {
     $(this).addClass("green");
   },function(){
     $(this).removeClass("green");
+  });
+  $("#faq dd").hover(function() {
+    old = $(this).html();
+    $(this).html("Pou");
+  }, function() {
+    $(this).html(old);
+  });
+  $("#faq dt").click(function() {
+    var nodo = $(this);
+    nodo.fadeOut("slow", function() {
+      $("h3").html(nodo.html());
+    });
+    $("h3").click(function() {
+      nodo.fadeIn("slow");
+    });
   });
 });
